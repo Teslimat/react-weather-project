cb9ed29b8e2dd92bf07be166b3bce243d2c9a023
Teslimat <teslimatadeyemi@gmail.com>
2024-04-04 12:17:15 +0100

    Included the footer element to the project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://teslimat-porfolio.netlify.app"
+          target="_blank"
+          rel="noreferrer">
+          Teslimat Adeyemi{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Teslimat/react-weather-project"
+          target="_blank"
+          rel="noreferrer"> open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
