9d9cc3e2a34fdcf6beabe793fa0ba0b9453850c1
Teslimat <teslimatadeyemi@gmail.com>
2024-04-08 11:50:13 +0100

    Updated the weather icon images using the react animated image

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,32 +1,35 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherInfo(props){
-    return (
-      <div className="WeatherInfor">
-        <h1>{props.data.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={props.data.dates} />
-          </li>
-          <li className="text-capitalize">{props.data.description}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <img src={props.data.iconUrl} alt={props.data.description} />
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfor">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.dates} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <WeatherIcon code={props.data.icon} />
 
             <span className="temperature">
               {Math.round(props.data.temperature)}
             </span>
             <span className="unit">°c</span>
           </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind}Km/h</li>
-            </ul>
-          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind}Km/h</li>
+          </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
